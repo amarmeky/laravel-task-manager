2d2b1faf7dd9ee0c9bf1726b731815dd77b78f35
Amar Meky <amarmeky7@gmail.com>
2025-07-06 20:03:39 +0300

    add

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('tasks',action: [TaskController::class,'index'] );
-Route::post('tasks',action: [TaskController::class,'store'] );
-Route::put('tasks/{id}',action: [TaskController::class,'update'] );
-Route::get('tasks/{id}',action: [TaskController::class,'show'] );
-Route::delete('tasks/{id}',action: [TaskController::class,'destroy'] ); 
+// Route::get('tasks',action: [TaskController::class,'index'] );
+// Route::post('tasks',action: [TaskController::class,'store'] );
+// Route::put('tasks/{id}',action: [TaskController::class,'update'] );
+// Route::get('tasks/{id}',action: [TaskController::class,'show'] );
+// Route::delete('tasks/{id}',action: [TaskController::class,'destroy'] ); 
+
+
+Route::apiResource('tasks', TaskController::class);
