4b041d7cd51f67af17a3949b5a5c9d66b87c1b63
Amar Meky <amarmeky7@gmail.com>
2025-07-08 15:53:39 +0300

    add function cascadeOnDelete() in profile

--- database/migrations/2025_07_08_082211_create_profiles_table.php
+++ database/migrations/2025_07_08_082211_create_profiles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->unique();
+            $table->foreignId('user_id')->constrained()->unique()->cascadeOnDelete();
             $table->string('name');
             $table->string('email');
             $table->string('phone')->nullable();
--- routes/api.php
+++ routes/api.php
@@ -10,15 +10,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Route::get('tasks',action: [TaskController::class,'index'] );
-// Route::post('tasks',action: [TaskController::class,'store'] );
-// Route::put('tasks/{id}',action: [TaskController::class,'update'] );
-// Route::get('tasks/{id}',action: [TaskController::class,'show'] );
-// Route::delete('tasks/{id}',action: [TaskController::class,'destroy'] ); 
-
-
 Route::apiResource('tasks', TaskController::class);
-Route::post('profile',[ProfileController::class,'store']);
-Route::get('profile/{id}',[ProfileController::class,'show']);
-Route::get('user/{id}/profile',[UserController::class,'getprofile']);
-Route::put('user/{id}/profile',[ProfileController::class,'updateprofile']);
+Route::post('profile', [ProfileController::class, 'store']);
+Route::get('profile/{id}', [ProfileController::class, 'show']);
+Route::get('user/{id}/profile', [UserController::class, 'getprofile']);
+Route::put('user/{id}/profile', [ProfileController::class, 'updateprofile']);
