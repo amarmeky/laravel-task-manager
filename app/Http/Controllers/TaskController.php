42572972057e37ad662009a6d5cdbbe5ebbca3bf
Amar Meky <amarmeky7@gmail.com>
2025-07-10 12:13:15 +0300

    add seeder and add storecategorytotaskrequest

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCategoryToTaskRequest;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
@@ -44,10 +45,10 @@ class TaskController extends Controller
         $user=Task::findOrFail($id)->user;
         return response()->json($user, 200);
     }
-    public function addCategorytoTask(Request $request, $id)
+    public function addCategorytoTask(StoreCategoryToTaskRequest $request, $id)
     {
         $task = Task::findOrFail($id);
-        $task->categories()->attach($request->category_id);
+        $task->categories()->attach($request->validated());
         return response()->json(['message' => 'Category added successfully'], 200);
     }
 }
