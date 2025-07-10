b803dc8986dcb87240c6498af2cc07205d3250ed
Amar Meky <amarmeky7@gmail.com>
2025-07-10 17:16:30 +0300

    add login and register and logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,37 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use App\Models\Task;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
+
+
+    public function register(RegisterRequest $request)
     {
-        $users=[
-            ["id"=>1,"name"=>"amar"],
-            ["id"=>2,"name"=>"hussien"],
-            ["id"=>3,"name"=>"hassan"],
-        ];
-        return response()->json($users);
-    }
-    public function checkuser(int $id){
-        if($id>10){
-            return response()->json(["message "=>"id is not vaild"]);
-        }else
-        return response()->json(["message"=>"id is vaild $id"]);
-    }
-    public function getprofile($id){
-        $profile=User::findOrFail($id)->profile;
-        return response()->json(['message'=>'Profile retrieved successfully',
-        'profile'=>$profile
+        $validate = $request->validated();
+        $validate['password'] = Hash::make($validate['password']);
+        $user = User::create($validate);
+        return response()->json([
+            'message' => ' user register successful',
+            'user' => $user
         ], 200);
     }
-    public function gettasks($id){
+
+    public function login(LoginRequest $request)
+    {
+        $validate = $request->validated();
+        if (!Auth::attempt($validate)) {
+            return response()->json(['message' => 'Invalid email or password'], 401);
+        }
+        $user = User::where('email', $validate['email'])->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'message' => 'Login successful',
+            'user' => $user,
+            'token' => $token
+        ], 200);
+    }
+
+public function logout(Request $request){
+$request->user()->tokens()->delete();
+return response()->json([
+    'message' => 'Logout successful'
+], 200);
+}
+
+    public function getprofile($id)
+    {
+        $profile = User::findOrFail($id)->profile;
+        return response()->json([
+            'message' => 'Profile retrieved successfully',
+            'profile' => $profile
+        ], 200);
+    }
+    public function gettasks($id)
+    {
         $tasks = User::findOrFail($id)->tasks;
-        return response()->json(['message' => 'Tasks retrieved successfully',
-        'tasks' => $tasks
+        return response()->json([
+            'message' => 'Tasks retrieved successfully',
+            'tasks' => $tasks
         ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,12 +10,19 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('register',[UserController::class,'register']);
+Route::post('login',[UserController::class,'login']);
+Route::post('logout',[UserController::class,'logout'])->middleware('auth:sanctum');
+
 Route::apiResource('tasks', TaskController::class);
-Route::post('profile', [ProfileController::class, 'store']);
-Route::get('profile/{id}', [ProfileController::class, 'show']);
-Route::get('user/{id}/profile', [UserController::class, 'getprofile']);
-Route::put('user/{id}/profile', [ProfileController::class, 'updateprofile']);
-Route::get('user/{id}/tasks', [UserController::class, 'gettasks']);
+Route::post('tasks/{id}/categories', [TaskController::class, 'addCategorytoTask']);
 Route::get('tasks/{id}/user', [TaskController::class, 'gettasksuser']);
 
-Route::post('tasks/{id}/categories', [TaskController::class, 'addCategorytoTask']);
+
+Route::post('profile', [ProfileController::class, 'store']);
+Route::get('profile/{id}', [ProfileController::class, 'show']);
+Route::put('user/{id}/profile', [ProfileController::class, 'updateprofile']);
+
+
+Route::get('user/{id}/profile', [UserController::class, 'getprofile']);
+Route::get('user/{id}/tasks', [UserController::class, 'gettasks']);
